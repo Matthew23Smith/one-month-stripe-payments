ae1c0cfcdedeac19f9ea4cb887a06fda1f7ebf0d
Matthew Smith <web.matthew.it@gmail.com>
2015-09-27 16:48:12 -0400

    adding ability to store purchases and card information

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ root 'pages#home'
 
 # stripe sources
 resources :charges
+#view purchase information
+resources :purchases, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
